bc34be51ef6b489611996652a6795bb069ee0c84
MIOC8 <105300798+MIOC8@users.noreply.github.com>
2022-07-22 00:59:25 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const ButtonHeader = document.querySelector(".burger__button");
+const Container = document.querySelector(".container");
+const BurgerListItems = document.querySelectorAll(".burger__list-item");
+ButtonHeader.addEventListener("click", function (e) {
+    ButtonHeader.classList.add("burger__button-active")
+    BurgerListItems.forEach(function (item) {
+        item.classList.add("burger__list-item-active")
+    })
+})
+document.addEventListener("click", function (e) {
+    if (!e.target.closest(".burger__button")) {
+        ButtonHeader.classList.remove("burger__button-active")
+        BurgerListItems.forEach(function (item) {
+            item.classList.remove("burger__list-item-active")
+        })
+    }
+})
